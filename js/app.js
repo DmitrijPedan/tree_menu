b7225f1c401338ce6a09b2fb0774a1a37aba59fa
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-02-07 23:33:57 +0200

    add while to parents

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('menu...')
+[...document.querySelectorAll('ul.list-unstyled.main ul')].map(el => el.classList.add('close'));
+
+const a = [...document.querySelectorAll('ul.list-unstyled.main a')];
+const ul = [...document.querySelectorAll('ul.list-unstyled.main ul')];
+
+const addSelectedClassToParents = (elem) => {
+  while (elem.parentNode.parentNode.className == "") {
+    elem = elem.parentNode.parentNode;
+    elem.nextElementSibling.classList.add('selected');
+}
+}
+
+a.map((el) => el.addEventListener('click', (event) => {
+  event.preventDefault();
+  a.map(el => el.classList.remove('selected'));
+  event.target.classList.add('selected');
+  addSelectedClassToParents(event.target);
+  ul.map(el => el.nextElementSibling.className == 'selected' ? el.classList.remove('close') : el.classList.add('close'));
+}));
