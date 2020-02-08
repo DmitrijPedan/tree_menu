4cb7f87e6d16304bdf30f209f3d856e235a621d4
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-02-08 17:07:43 +0200

    add category items

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 [...document.querySelectorAll('ul.list-unstyled.main ul')].map(el => el.classList.add('close'));
-
 const a = [...document.querySelectorAll('ul.list-unstyled.main a')];
 const ul = [...document.querySelectorAll('ul.list-unstyled.main ul')];
 
@@ -10,12 +9,17 @@ const addSelectedClassToParents = (elem) => {
   }
 }
 
-const addPath = (elem) => {
-  document.getElementById('path').innerHTML = '';
+const getPath = (elem) => {
+  document.getElementById('pathOutput').innerHTML = '';
   while (elem.parentNode.parentNode.nextElementSibling) {
-    elem.classList == 'selected' ? document.getElementById('path').prepend(' / ',elem.textContent) : document.getElementById('path').innerHTML = '';
+    elem.classList == 'selected' ? document.getElementById('pathOutput').prepend(' / ',elem.textContent) : document.getElementById('pathOutput').innerHTML = '';
     elem = elem.parentNode.parentNode.nextElementSibling; 
   }
+}
+
+const getContentOfCategory = (elem) => {
+  document.getElementById('contentOutput').innerHTML = '';
+  !elem.previousElementSibling && elem.classList.contains("selected") ? document.getElementById('contentOutput').append('Items of ',elem.textContent) : null;
 }
 
 a.map((el) => el.addEventListener('click', (event) => {
@@ -27,7 +31,7 @@ a.map((el) => el.addEventListener('click', (event) => {
     event.target.classList.add('selected')
   }
   addSelectedClassToParents(event.target);
-  addPath(event.target);
+  getPath(event.target);
+  getContentOfCategory(event.target);
   ul.map(el => el.nextElementSibling.className == 'selected' ? el.classList.remove('close') : el.classList.add('close'));
 }));
-
