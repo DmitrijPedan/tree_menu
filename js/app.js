3595a776250b0c1e4ef562415301953f45e5ab1a
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-02-08 13:45:09 +0200

    add path

--- js/app.js
+++ js/app.js
@@ -7,13 +7,27 @@ const addSelectedClassToParents = (elem) => {
   while (elem.parentNode.parentNode.className == "") {
     elem = elem.parentNode.parentNode;
     elem.nextElementSibling.classList.add('selected');
+  }
 }
+
+const addPath = (elem) => {
+  document.getElementById('path').innerHTML = '';
+  while (elem.parentNode.parentNode.nextElementSibling) {
+    elem.classList == 'selected' ? document.getElementById('path').prepend(' / ',elem.textContent) : document.getElementById('path').innerHTML = '';
+    elem = elem.parentNode.parentNode.nextElementSibling; 
+  }
 }
 
 a.map((el) => el.addEventListener('click', (event) => {
   event.preventDefault();
-  a.map(el => el.classList.remove('selected'));
-  event.target.classList.add('selected');
+  if (event.target.classList.contains('selected')) {
+    event.target.classList.remove('selected')
+  } else {
+    a.map(el => el.classList.remove('selected'));
+    event.target.classList.add('selected')
+  }
   addSelectedClassToParents(event.target);
+  addPath(event.target);
   ul.map(el => el.nextElementSibling.className == 'selected' ? el.classList.remove('close') : el.classList.add('close'));
 }));
+
